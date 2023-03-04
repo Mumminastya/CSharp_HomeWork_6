9221a5a05f449e27a3c64b699197b0c02f762584
Anastasia Kotlyarova <mumminastya@gmail.com>
2023-03-05 01:55:30 +0300

    HomeWork6

---
+++ task41/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] Promt(string message)
+{
+    Console.WriteLine(message);
+    var result = Console.ReadLine();
+    var splitted = result.Split(",");
+    int[] NewMassiv = new int[splitted.Length];
+    for (int i = 0; i < splitted.Length; i++)
+    {
+        NewMassiv[i] = Convert.ToInt32(splitted[i]);
+    }
+    return NewMassiv;
+}
+
+void PrintMasssiv(int[] NewMassiv, int result)
+{
+    Console.WriteLine(string.Join(",",NewMassiv) + "->" + result.ToString());
+}
+
+
+int NumberMoreNule(int[] result)
+{
+   int count = 0;
+   for(int i = 0; i < result.Length; i++)
+   {
+    if(result[i] > 0)
+    {
+        count++;
+    }
+    }
+   return count;
+}
+
+int[] arr = Promt("Введите числа, сколько хотите: ");
+int result = NumberMoreNule(arr);
+PrintMasssiv(arr, result);
---
+++ task43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1,
+// y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int Promt(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void IntersectionPoint(int b1, int k1, int b2, int k2)
+{
+    double x = (b2 * 1.0 - b1) / (k1 - k2);
+    double y = k1*x + b1;
+    string r = $"b1={b1}, k1={k1}, b2={b2}, k2={k2} -> ({x};{y})";
+    Console.WriteLine(r);
+}
+
+int b1 = Promt("Введите значение b1: ");
+int k1 = Promt("Введите значение k1: ");
+int b2 = Promt("Введите значение b2: ");
+int k2 = Promt("Введите значение k2: ");
+IntersectionPoint(b1, k1, b2, k2);
